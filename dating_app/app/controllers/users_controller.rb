412b30025f08f5bc1bfd4a296665219ab06e8bcc
FRANCIUIPAPI <franciuipapi@gmail.com>
2014-09-19 17:48:51 +0100

    finishing association user-message

--- dating_app/app/controllers/users_controller.rb
+++ dating_app/app/controllers/users_controller.rb
@@ -14,6 +14,8 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
     @user = User.find(params[:id])
+    @messages = Message.all
+    # user.messages_as_receiver+user.messages_as_sender
 
     respond_to do |format|
       format.html # show.html.erb
--- dating_app/app/models/user.rb
+++ dating_app/app/models/user.rb
@@ -5,6 +5,6 @@ has_many :views_as_viewer, class_name:'Game', foreign_key: 'viewer_id'
 has_many :views_as_viewed, class_name:'Game', foreign_key: 'viewed_id'
 
 has_many :messages_as_sender, class_name:'Game', foreign_key: 'sender_id'
-has_many :views_as_receiver, class_name:'Game', foreign_key: 'receiver_id'
+has_many :messages_as_receiver, class_name:'Game', foreign_key: 'receiver_id'
  
 end
