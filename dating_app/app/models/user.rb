f40946ccd44eb59fa46f81fcd99ae23517969947
FRANCIUIPAPI <franciuipapi@gmail.com>
2014-09-19 16:56:36 +0100

    updating has many relationships for messages and views

--- dating_app/app/models/user.rb
+++ dating_app/app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ActiveRecord::Base
   attr_accessible :about_me, :age, :city, :email, :eyes_color, :favourite_language, :gender, :hair_color, :height, :name, :sex_preference, :smoker
 
-has_many :likes
-has many :messages
+has_many :views_as_viewer, class_name:'Game', foreign_key: 'viewer_id'
+has_many :views_as_viewed, class_name:'Game', foreign_key: 'viewed_id'
 
+has_many :messages_as_sender, class_name:'Game', foreign_key: 'sender_id'
+has_many :views_as_receiver, class_name:'Game', foreign_key: 'receiver_id'
+ 
 end
