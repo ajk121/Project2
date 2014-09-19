6a86dd7f4cc5634dfe0118c30020f2a29e8f9dba
FRANCIUIPAPI <franciuipapi@gmail.com>
2014-09-19 17:20:50 +0100

    creating belogs_to association for Message and View

--- dating_app/app/models/view.rb
+++ dating_app/app/models/view.rb
@@ -1,3 +1,7 @@
 class View < ActiveRecord::Base
   attr_accessible :viewed_id, :viewer_id
+
+belongs_to :viewer, class_name: 'User', foreign_key: 'viewer_id'
+belongs_to :viewed, class_name: 'User', foreign_key: 'viewed_id'
+
 end
