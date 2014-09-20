8732c437d6d0253e6ea91f9253d51ba6371ee06d
FRANCIUIPAPI <franciuipapi@gmail.com>
2014-09-20 11:08:28 +0100

    m adding image and username fields for user

--- dating_app/app/models/user.rb
+++ dating_app/app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
 
-  attr_accessible :about_me, :age, :city, :eyes_color, :favourite_language, :gender, :hair_color, :height, :name, :sex_preference, :smoker
+  attr_accessible :about_me, :age, :city, :eyes_color, :favourite_language, :gender, :hair_color, :height, :name, :sex_preference, :smoker, :image, :username
 
 has_many :views_as_viewer, class_name:'View', foreign_key: 'viewer_id', dependent: :destroy
 has_many :views_as_viewed, class_name:'View', foreign_key: 'viewed_id', dependent: :destroy
--- dating_app/db/schema.rb
+++ dating_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140920082136) do
+ActiveRecord::Schema.define(:version => 20140920094623) do
 
   create_table "messages", :force => true do |t|
     t.string   "title"
@@ -48,6 +48,8 @@ ActiveRecord::Schema.define(:version => 20140920082136) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "image"
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email"
