bb69e311fb7267e4eabdcb20525b2da00ba0d23d
FRANCIUIPAPI <franciuipapi@gmail.com>
2014-09-20 09:46:31 +0100

    adding dependencies to messages and views

--- dating_app/app/controllers/users_controller.rb
+++ dating_app/app/controllers/users_controller.rb
@@ -14,8 +14,8 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
     @user = User.find(params[:id])
-    @messages = Message.all
-    # user.messages_as_receiver+user.messages_as_sender
+    @messages = @user.messages_as_receiver + @user.messages_as_sender
+    @views = @user.views_as_viewed + @user.views_as_viewer
 
     respond_to do |format|
       format.html # show.html.erb
--- dating_app/app/models/user.rb
+++ dating_app/app/models/user.rb
@@ -9,10 +9,10 @@ class User < ActiveRecord::Base
 
   attr_accessible :about_me, :age, :city, :eyes_color, :favourite_language, :gender, :hair_color, :height, :name, :sex_preference, :smoker
 
-has_many :views_as_viewer, class_name:'Game', foreign_key: 'viewer_id'
-has_many :views_as_viewed, class_name:'Game', foreign_key: 'viewed_id'
+has_many :views_as_viewer, class_name:'View', foreign_key: 'viewer_id', dependent: :destroy
+has_many :views_as_viewed, class_name:'View', foreign_key: 'viewed_id', dependent: :destroy
 
-has_many :messages_as_sender, class_name:'Game', foreign_key: 'sender_id'
-has_many :messages_as_receiver, class_name:'Game', foreign_key: 'receiver_id'
+has_many :messages_as_sender, class_name:'Message', foreign_key: 'sender_id', dependent: :destroy
+has_many :messages_as_receiver, class_name:'Message', foreign_key: 'receiver_id',dependent: :destroy
  
 end
