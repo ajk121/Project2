e5fd3fc4ac9ea4ded1deb617596abc9115ba5ed9
FRANCIUIPAPI <franciuipapi@gmail.com>
2014-09-21 12:36:22 +0100

    adding message block for basic users with cancan

---
+++ dating_app/app/models/ability.rb
@@ -0,0 +1,21 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new
+    if user.role? ==:admin
+     can :manage, :all
+    elsif user.role? ==:premium
+     can [:read, :update], User, id: user.id
+     can [:read, :create], Message
+     can :read, View
+     can :read, :home
+    else
+     can :read, View
+     can :create, User
+     cannot [:read, :create], Message
+     can [:read, :update], User, id: user.id
+     can :read, :home
+    end
+  end
+end
