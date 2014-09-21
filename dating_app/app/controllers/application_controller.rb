b9e1b88fe1f92321e14d7018bbcb1c7932f0e279
FRANCIUIPAPI <franciuipapi@gmail.com>
2014-09-21 12:07:51 +0100

    adding Ability model and working on cancan authorizations

--- dating_app/app/controllers/application_controller.rb
+++ dating_app/app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:error] = "Sorry, you cannot acces this page!"
+    redirect_to root_url
+  end
+  helper_method :current_user
+
+
+
 end
