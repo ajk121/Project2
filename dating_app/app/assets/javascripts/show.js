1fbb37c801ef323ecc192b899cbc21a56a4ed486
FRANCIUIPAPI <franciuipapi@gmail.com>
2014-09-22 20:18:12 +0100

    adding js to images

--- dating_app/app/assets/javascripts/show.js
+++ dating_app/app/assets/javascripts/show.js
@@ -1,4 +1,13 @@
-// $(document).ready(function(){
+$(document).ready(function(){
 
+  $('.search_image').mouseenter(function(ev) {
+    var userid = $( ev.currentTarget ).data('user-id');
+    $("#image_name_"+userid).slideDown();
+  });
 
-// });
+  $('.search_image').mouseleave(function(ev) {
+    var userid = $( ev.currentTarget ).data('user-id');
+    $("#image_name_"+userid).slideUp();
+  });
+
+});
