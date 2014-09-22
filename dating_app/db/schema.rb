d722961caae82897e87257e21ef85c09ceb2ade7
ajk121 <contact.abdulkhan@gmail.com>
2014-09-22 18:23:21 +0100

    adding pics and fields

--- dating_app/db/schema.rb
+++ dating_app/db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(:version => 20140922152407) do
-=======
-ActiveRecord::Schema.define(:version => 20140922144552) do
->>>>>>> 3e725cf5173d5b52ba2433c624175ef701f40a2d
+ActiveRecord::Schema.define(:version => 20140922165117) do
 
   create_table "impressions", :force => true do |t|
     t.string   "impressionable_type"
@@ -72,8 +68,6 @@ ActiveRecord::Schema.define(:version => 20140922144552) do
     t.string   "about_me"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
-    t.string   "provider"
-    t.string   "uid"
     t.string   "email",                  :default => "", :null => false
     t.string   "encrypted_password",     :default => "", :null => false
     t.string   "reset_password_token"
@@ -90,6 +84,7 @@ ActiveRecord::Schema.define(:version => 20140922144552) do
     t.string   "role"
     t.string   "meet_ups"
     t.string   "status"
+    t.string   "front_backend"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email"
