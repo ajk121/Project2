cb7a4b0dc1faad2105d5cc09455f401f5aa655ce
FRANCIUIPAPI <franciuipapi@gmail.com>
2014-09-23 10:44:33 +0100

    fixing merging

--- dating_app/app/helpers/users_helper.rb
+++ dating_app/app/helpers/users_helper.rb
@@ -1,15 +1,3 @@
 module UsersHelper
 
-#   def age
-#     case 
-#     when age > 18 && <= 23
-#     when age >= 24 && <= 29
-#     when age >= 30 && <= 35
-#     when age >= 36 && <= 40
-#     when age >= 40 && <= 45
-#     when age >= 45 && <= 50
-#     when age >= 50 && <= 55
-#     when age >= 55
-#     end
-
   end
