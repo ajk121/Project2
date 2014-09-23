80d9094278ea6c5e915a5371564bf6c6cc9a8624
FRANCIUIPAPI <franciuipapi@gmail.com>
2014-09-23 23:49:46 +0100

    finishing search dropdown

--- dating_app/app/assets/javascripts/show.js
+++ dating_app/app/assets/javascripts/show.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
 
-  $('.search_image').mouseenter(function(ev) {
-    var userid = $( ev.currentTarget ).data('user-id');
-    $("#image_name_"+userid).slideDown();
-  });
+  // $('.search_image').mouseenter(function(ev) {
+  //   var userid = $( ev.currentTarget ).data('user-id');
+  //   $("#image_name_"+userid).slideDown();
+  // });
 
-  $('.search_image').mouseleave(function(ev) {
-    var userid = $( ev.currentTarget ).data('user-id');
-    $("#image_name_"+userid).slideUp();
-  });
+  // $('.search_image').mouseleave(function(ev) {
+  //   var userid = $( ev.currentTarget ).data('user-id');
+  //   $("#image_name_"+userid).slideUp();
+  // });
   
   $( '.dropdown' ).hover(
     function(){
