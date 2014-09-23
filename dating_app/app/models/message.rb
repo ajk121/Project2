3f76640a614436b6dff360aaf93f39b1940fff48
ajk121 <contact.abdulkhan@gmail.com>
2014-09-23 12:09:50 +0100

    created status column in database, added read/unread within status

--- dating_app/app/models/message.rb
+++ dating_app/app/models/message.rb
@@ -4,5 +4,12 @@ class Message < ActiveRecord::Base
 belongs_to :sender, class_name: 'User', foreign_key: 'sender_id'
 belongs_to :receiver, class_name: 'User', foreign_key: 'receiver_id'
 
+ before_validation :set_default_status
+
+ private 
+ def set_default_status
+  self.status ||= 'unread'
+ end
+  
 
 end
