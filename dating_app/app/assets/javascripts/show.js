8370a2bf8dde98db269cfd06bde258e20525ed37
FRANCIUIPAPI <franciuipapi@gmail.com>
2014-09-23 22:01:35 +0100

    finishing dropdown button for user profile

--- dating_app/app/assets/javascripts/show.js
+++ dating_app/app/assets/javascripts/show.js
@@ -1,13 +1,20 @@
 $(document).ready(function(){
 
-  $('.search_image').mouseenter(function(ev) {
-    var userid = $( ev.currentTarget ).data('user-id');
-    $("#image_name_"+userid).slideDown();
-  });
+  // $('.search_image').mouseenter(function(ev) {
+  //   var userid = $( ev.currentTarget ).data('user-id');
+  //   $("#image_name_"+userid).slideDown();
+  // });
 
-  $('.search_image').mouseleave(function(ev) {
-    var userid = $( ev.currentTarget ).data('user-id');
-    $("#image_name_"+userid).slideUp();
-  });
-
+  // $('.search_image').mouseleave(function(ev) {
+  //   var userid = $( ev.currentTarget ).data('user-id');
+  //   $("#image_name_"+userid).slideUp();
+  // });
+$( '.dropdown' ).hover(
+        function(){
+            $(this).children('.sub-menu').slideDown(300);
+        },
+        function(){
+            $(this).children('.sub-menu').slideUp(300);
+        }
+    );
 });
