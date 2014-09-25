2d57348e793df0082098ea92165eda2c1b5792e6
LeeWayne <lee.wayne1882@googlemail.com>
2014-09-25 13:32:35 +0100

    my visits and people visited picture fix

--- dating_app/app/controllers/views_controller.rb
+++ dating_app/app/controllers/views_controller.rb
@@ -1,13 +1,15 @@
 class ViewsController < ApplicationController
   
+  # I'm looking at someone else
   def user_makes_visit
     @user = current_user
-    @users = Kaminari.paginate_array(@user.views_as_viewer.map(&:viewer)).page(params[:page])
+    @users = Kaminari.paginate_array(@user.views_as_viewer.map(&:viewed).reverse).page(params[:page])
   end
 
+  # People looking at me
   def user_receives_visit
     @user = current_user
-    @users = Kaminari.paginate_array(@user.views_as_viewed.map(&:viewed)).page(params[:page])
+    @users = Kaminari.paginate_array(@user.views_as_viewed.map(&:viewer)).page(params[:page])
   end
 
   def destroy
