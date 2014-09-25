a84d797af7b22498a7b177346ff0c3cda51fdddd
FRANCIUIPAPI <franciuipapi@gmail.com>
2014-09-25 16:34:33 +0100

    fixing issue with login

--- dating_app/app/controllers/users_controller.rb
+++ dating_app/app/controllers/users_controller.rb
@@ -33,14 +33,14 @@ class UsersController < ApplicationController
 
   # GET /users/new
   # GET /users/new.json
-  def new
-    @user = User.new
+  # def new
+  #   @user = User.new
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @user }
-    end
-  end
+  #   respond_to do |format|
+  #     format.html # new.html.erb
+  #     format.json { render json: @user }
+  #   end
+  # end
 
   # GET /users/1/edit
   def edit
