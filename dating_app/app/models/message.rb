0deb218c2ddf470206278f1bb2b672c1e1078150
ajk121 <contact.abdulkhan@gmail.com>
2014-09-26 11:35:41 +0100

    making changes to messages

--- dating_app/app/models/message.rb
+++ dating_app/app/models/message.rb
@@ -1,5 +1,6 @@
 class Message < ActiveRecord::Base
-  
+  default_scope order: 'created_at DESC'
+
   attr_accessible :content, :receiver_id, :sender_id, :title
 
   belongs_to :sender, class_name: 'User', foreign_key: 'sender_id'
